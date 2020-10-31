3978737cdd13ea4d7e97c7d6c8c2cda79253b32c
pepya4ka <ndrozdov9@gmail.com>
2020-10-31 19:53:47 +0300

    first commit

---
+++ src/main/java/com/pepyachka/myspringbootwebsocketexample/MyspringbootwebsocketexampleApplication.java
@@ -0,0 +1,13 @@
+package com.pepyachka.myspringbootwebsocketexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyspringbootwebsocketexampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyspringbootwebsocketexampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pepyachka/myspringbootwebsocketexample/MyspringbootwebsocketexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pepyachka.myspringbootwebsocketexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyspringbootwebsocketexampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
