b3494a055736c5285718f014648638b02b931235
sausageRoll <drozdovigor1996@gmail.com>
2020-11-11 17:44:24 +0100

    Add simple test

--- src/main/java/com/pepyachka/myspringbootwebsocketexample/model/GameState.java
+++ src/main/java/com/pepyachka/myspringbootwebsocketexample/model/GameState.java
@@ -1,4 +1,20 @@
 package com.pepyachka.myspringbootwebsocketexample.model;
 
 public class GameState {
+    private int value;
+
+    public GameState() {
+    }
+
+    public GameState(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
 }
