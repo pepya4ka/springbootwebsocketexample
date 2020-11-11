e6885264683ecf0140f72ce6554750ab93ed384a
pepya4ka <ndrozdov9@gmail.com>
2020-11-11 13:25:35 +0300

    add controllers

---
+++ game-service/src/main/java/com/pepyachka/myspringbootwebsocketexample/controller/MainController.java
@@ -0,0 +1,27 @@
+package com.pepyachka.myspringbootwebsocketexample.controller;
+
+import com.pepyachka.myspringbootwebsocketexample.model.GameState;
+import com.pepyachka.myspringbootwebsocketexample.model.PlayerStat;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MainController {
+
+    @MessageMapping("/main.start")
+    @SendTo("/game")
+    public GameState start() {
+        return new GameState();
+    }
+
+    @MessageMapping("/main.stat")
+    public PlayerStat stat() {
+        return new PlayerStat();
+    }
+
+    @MessageMapping("/main.search")
+    public void search() {
+        return;
+    }
+}
